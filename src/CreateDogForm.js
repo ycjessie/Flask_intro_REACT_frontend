7d73c2b90a2d313b5e335607b311c948e55dfcc1
Jessie Chen <yhcjessie1@gmail.com>
2020-12-08 15:16:27 -0500

    clear form after add a dog-add required on the form fields

--- src/CreateDogForm.js
+++ src/CreateDogForm.js
@@ -18,13 +18,18 @@ class CreateDogForm extends Component {
         return (
           <Segment>
             <h4>Create Dog</h4>
-            <Form onSubmit={(e) => this.props.addDog(e, this.state)}>
+            
+            <Form onSubmit={(e) => {
+                this.props.addDog(e, this.state);
+                // clear the form after add the dog
+                this.setState({ name: '', owner: '', breed: '' });
+                }}>
               <Label>Dog:</Label>
-              <Form.Input type='text' name='name' value={this.state.name} onChange={this.handleChange}/>
+              <Form.Input required type='text' name='name' value={this.state.name} onChange={this.handleChange}/>
               <Label>Owner:</Label>
-              <Form.Input type='text' name='owner' value={this.state.owner} onChange={this.handleChange}/>
+              <Form.Input reqired type='text' name='owner' value={this.state.owner} onChange={this.handleChange}/>
               <Label>Breed:</Label>
-              <Form.Input type='text' name='breed' value={this.state.breed} onChange={this.handleChange}/>
+              <Form.Input required type='text' name='breed' value={this.state.breed} onChange={this.handleChange}/>
               <Button type='Submit'>Create Dog</Button>
             </Form>
           </Segment>
